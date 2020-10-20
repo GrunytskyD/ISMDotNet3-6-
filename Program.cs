8667a4971a20e81e50069f06bb725782873cdf3d
Dmytro Grunytsky <dima.grunitsky@gmail.com>
2020-10-20 17:20:17 +0300

    Кількість цілих чисел у масиві

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //сума від'ємних елементів масива
+            int A[30];
+            int suma;//змінна,що містить суму
+            int i;//додаткова змінна
+                  //Введення масива 
+                  //...
+
+            // Розв'язок 
+            suma = 0;
+            for (i = 0; i < 30; i++)
+                if (A[i] > 0)
+                    suma = suma + A[i];
+        }
+    }
+}
